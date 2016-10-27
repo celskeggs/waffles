d0db24f7fee5824fd1daad1758a03f5b7ec64fe1
Cel Skeggs <git@celskeggs.com>
2016-10-27 00:08:13 -0400

    Initial version

---
+++ init_disks.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+set -e -x
+
+if [ "$1" = "" -o "$2" = "" ]
+then
+	echo "Usage: $0 sda <pass>" 1>&2
+	exit 1
+fi
+
+DISK=/dev/$1
+PASS=$2
+dd if=/dev/zero of=$DISK count=16
+# TODO: swap space
+echo "n p 1 2048 +300M" "n p 2 616448 " "w" | tr " " "\n" | fdisk $DISK
+echo "YES\n$PASS\n$PASS" | cryptsetup -y luksFormat "$DISK"2
+echo "$PASS" | cryptsetup open "$DISK"2 cryptroot
+mkfs -t ext4 /dev/mapper/cryptroot
+mkfs -t ext4 -O '^64bit' "$DISK"1
+mount -t ext4 /dev/mapper/cryptroot /mnt
+mkdir /mnt/boot
+mount -t ext4 "$DISK"1 /mnt/boot
+# TODO: select better mirror set
+pacstrap /mnt base
+genfstab -U /mnt >> /mnt/etc/fstab
+cp $(dirname $0)/init_inside.sh /mnt
+arch-chroot /mnt /init_inside.sh
+cp syslinux.cfg.default /mnt/boot/syslinux/syslinux.cfg
+sed -i "s/{{UUID}}/$(lsblk -f "$DISK"2 --output UUID | tail -n 1)/g" /mnt/boot/syslinux/syslinux.cfg
+# TODO: network setup
---
+++ init_inside.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+set -e -x
+
+if [ "$1" = "" ]
+then
+	echo "Usage: $0 <pass>" 1>&2
+	exit 1
+fi
+
+PASS=$1
+
+ln -s /usr/share/zoneinfo/America/New_York /etc/localtime
+hwclock --systohc
+sed -i "s/#en_US[.]UTF-8/en_US.UTF-8/" /etc/locale.gen
+locale-gen
+echo "LANG=en_US.UTF-8" >/etc/locale.conf
+echo "KEYMAP=dvorak" >/etc/vconsole.conf
+echo "ccu-$(od -t x1 /dev/urandom -N 2 -A n | sed 's/ //g')" >/etc/hostname
+pacman -S --noconfirm wpa_supplicant iw dialog sudo syslinux polkit
+useradd -m -G wheel,adm,rfkill,log,sys,systemd-journal,users,uucp,lp -s /bin/bash cela
+echo "cela:$PASS" | chpasswd
+echo "cela ALL=(ALL) ALL" >/etc/sudoers.d/cela
+sed -i 's/ keyboard//' /etc/mkinitcpio.conf
+sed -i 's/filesystems/keyboard keymap encrypt filesystems/' /etc/mkinitcpio.conf
+mkinitcpio -p linux
+syslinux-install_update -i -a -m
